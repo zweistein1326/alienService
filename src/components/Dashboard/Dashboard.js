c45de3550186c8f90887b1b81043084f7226cb93
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-08-01 00:55:35 +0530

    added Header and Dashboard

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="home">
+      
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="App-header">
+      <h1>Alienated</h1>
+    </nav>
+  );
+};
+
+export default Header;
