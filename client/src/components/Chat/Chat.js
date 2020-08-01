adaf07188b4fe4742b3844f6e0bc6d2a320bdff2
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-08-01 22:02:30 +0530

    reset

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from "react";
-// import queryString from "query-string";
-// import io from 'socket.io-client'
+import queryString from "query-string";
+import io from "socket.io-client";
 import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import "./Chat.css";
+
+let socket;
 
 const Chat = () => {
   return (
     <div>
       <Header />
       <Dashboard />
+      {/* <div className="outerContainer">
+        <div className="innerContainer">
+          <input
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            onKeyPress={(event) =>
+              event.key === "Enter" ? sendMessage(event) : null
+            }
+          />
+        </div>
+      </div> */}
     </div>
   );
 };
--- client/src/components/ChatBox/ChatBox.js
+++ client/src/components/ChatBox/ChatBox.js
@@ -1,9 +1,47 @@
-import React from "react"
-import { Box, Button, Flex, Textarea } from "@chakra-ui/core"
-import "./ChatBox.css"
-import { IoIosRocket } from "react-icons/io"
+import React, { useState, useEffect } from "react";
+import { Box, Button, Input } from "@chakra-ui/core";
+import "./ChatBox.css";
+import { IoIosRocket } from "react-icons/io";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+let socket;
 
 const ChatBox = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const ENDPOINT = "localhost:5000";
+  useEffect(() => {
+    const { name, room } = queryString.parse(window.location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {});
+    return () => {
+      socket.emit("disconnect");
+
+      socket.off();
+    };
+  }, [ENDPOINT, window.location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+  console.log(message, messages);
   return (
     <Box
       height="80%"
@@ -17,27 +55,31 @@ const ChatBox = () => {
         This is the chat box
       </Box>
       <Box height="30%" position="relative">
-        <Textarea
+        <Input
+          value={message}
           placeholder="Say Hello!"
           width="50vw"
           className="chatText"
-        ></Textarea>
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        ></Input>
         <Button
-          variantColor="beige "
-          color="white"
           height="32%"
           width="100%"
           position="relative"
           bottom="5px"
           backgroundColor="#00b280"
           className="button"
+          // onClick={(event) => setMessage(message)}
         >
           Send
           <IoIosRocket className="logoShip" />
         </Button>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default ChatBox
+export default ChatBox;
