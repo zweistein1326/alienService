b0ec420cec0e28af326bf2abbb70c9f99240daa6
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-08-02 00:14:36 +0530

    major updates

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,27 +2,73 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 import Header from "../Header/Header";
-import Dashboard from "../Dashboard/Dashboard";
 import "./Chat.css";
+import "../ChatBox/ChatBox.css";
+import VideoBox from "../VideoBox/VideoBox";
+import { Flex, Box } from "@chakra-ui/core";
+import InputColumn from "../ChatBox/Input";
 
 let socket;
 
 const Chat = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const ENDPOINT = "localhost:5000";
+  useEffect(() => {
+    const { name, room } = queryString.parse(window.location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {});
+    return () => {
+      socket.emit("disconnect");
+
+      socket.off();
+    };
+  }, [ENDPOINT, window.location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+  console.log(message, messages);
+
   return (
     <div>
-      <Header />
-      <Dashboard />
-      {/* <div className="outerContainer">
-        <div className="innerContainer">
-          <input
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-            onKeyPress={(event) =>
-              event.key === "Enter" ? sendMessage(event) : null
-            }
+      <Header room={room} />
+      <Flex>
+        <VideoBox />
+        <Box
+          height="80vh"
+          width="100%"
+          margin="2rem"
+          alignItems="space-between"
+          justifyContent="center"
+          className="outer"
+        >
+          <Box height="70%" width="100%" className="chat">
+            Chat Box
+          </Box>
+          <InputColumn
+            message={message}
+            setMessage={setMessage}
+            sendMessage={sendMessage}
           />
-        </div>
-      </div> */}
+        </Box>
+      </Flex>
     </div>
   );
 };
