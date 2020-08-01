293a15d88b03f857d144890065b61fc36d910f76
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-08-01 18:08:37 +0530

    add server functionality

---
+++ client/src/components/Chat.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header/Header";
+import Dashboard from "./Dashboard/Dashboard";
+
+const Chat = () => {
+  return (
+    <div>
+      <Header />
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Chat;
