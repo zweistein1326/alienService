889c5cba42e29123722a2b6de242f1c2c4dc3dff
Pranay Agarwal <pranay12@connect.hku.hk>
2020-08-01 17:32:41 +0530

    Chatbox

--- client/src/components/VideoBox/VideoBox.js
+++ client/src/components/VideoBox/VideoBox.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box } from "@chakra-ui/core";
+import React from "react"
+import { Box } from "@chakra-ui/core"
 
 const VideoBox = () => {
   return (
@@ -9,6 +9,8 @@ const VideoBox = () => {
       margin="2rem"
       alignItems="space-between"
       justifyContent="center"
+      backgroundColor="#f2f5f2"
+      borderColor="green"
     >
       <Box
         border="2px solid black"
@@ -20,10 +22,10 @@ const VideoBox = () => {
         User image goes here
         <Box
           border="2px solid black"
-          height="40%"
-          width="45%"
-          bottom="0"
-          right="0"
+          height="28%"
+          width="40%"
+          bottom="7px"
+          right="2"
           position="absolute"
         >
           User2 image goes here
@@ -38,7 +40,7 @@ const VideoBox = () => {
         Option buttons go here
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default VideoBox;
+export default VideoBox
