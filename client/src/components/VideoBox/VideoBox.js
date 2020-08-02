d92518b3c2c6845fbae8cba9c3a9f7f9d3476e7c
Anuj Bhatia <anuj@connect.hku.hk>
2020-08-02 13:43:13 +0530

    UI Updates

--- client/src/components/VideoBox/VideoBox.js
+++ client/src/components/VideoBox/VideoBox.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { Box } from "@chakra-ui/core";
 import Webcam from "react-webcam";
+import { RiCameraLine, RiCameraOffLine, RiAliensLine} from 'react-icons/ri';
+import { FiMic, FiMicOff } from 'react-icons/fi';
+import { MdFace } from 'react-icons/md';
+import './VideoBox.css';
 
 class VideoBox extends React.Component {
   state = {
     openCamera: false,
+    openMic: false,
+    openFilter: false,
   }
   cameraOn = () => {
     this.setState({
       openCamera: !this.state.openCamera
     })
   }
+  muteOn = () => {
+    this.setState({
+      openMic: !this.state.openMic
+    })
+  }
+  alienOn = () => {
+    this.setState({
+      openFilter: !this.state.openFilter
+    })
+  }
   render() {
-    const {openCamera} = this.state
+    const {openCamera,openMic, openFilter} = this.state
     return (
       <Box
         height="80vh"
@@ -30,8 +46,7 @@ class VideoBox extends React.Component {
           marginBottom="0"
           position="relative"
         >
-          {openCamera ? <Webcam mirrored="true" imageSmoothing="true" /> : <div><p>Please Turn On your Video</p><br/></div>}
-          <button onClick={this.cameraOn}>{openCamera ? "Turn Camera Off" : 'Turn Camera On'}</button>
+          {openCamera ? <Webcam mirrored="true" imageSmoothing="true" size="40" height="80%" width="80%"/> : <div><p>Please Turn On your Video</p><br/></div>}
           <Box
             border="2px solid black"
             height="28%"
@@ -45,11 +60,19 @@ class VideoBox extends React.Component {
         </Box>
         <Box
           border="2px solid black"
-          height="10%"
+          height="12%"
           width="100%"
           position="relative"
+          opacity="none"
+          alignItems="space-between"
         >
-          Option buttons go here
+        <div className="spread">
+        <a onClick={this.cameraOn}>{openCamera ? <RiCameraOffLine size="40" className="logo-endcall" /> : <RiCameraLine size="40" className="logo-endcall" />}</a>
+        &emsp;&emsp;&emsp;&emsp;
+        <a onClick={this.muteOn}>{openMic ? <FiMicOff size="40" className="logo-endcall" /> : <FiMic size="40" className="logo-endcall" />}</a>
+        &emsp;&emsp;&emsp;&emsp;
+        <a onClick={this.alienOn}>{openFilter ? <RiAliensLine size="40" className="logo-endcall" /> : <MdFace size="40" className="logo-endcall" />}</a>
+        </div>
       </Box>
       </Box>
     );
