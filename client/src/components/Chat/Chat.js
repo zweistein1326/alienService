e72ddb4f0257ca3a9666189a5c537dc7672b04a8
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-08-02 12:42:22 +0530

    updates

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
   const [users, setUsers] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://alien8.herokuapp.com/";
   useEffect(() => {
     const { name, room } = queryString.parse(window.location.search);
 
