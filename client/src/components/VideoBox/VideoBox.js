f280067ecb594e632b817e6b392ad6888e0889bd
Anuj Bhatia <anuj@connect.hku.hk>
2020-08-02 12:09:59 +0530

    Webcam Update

--- client/src/components/VideoBox/VideoBox.js
+++ client/src/components/VideoBox/VideoBox.js
@@ -1,46 +1,59 @@
 import React from "react";
 import { Box } from "@chakra-ui/core";
+import Webcam from "react-webcam";
 
-const VideoBox = () => {
-  return (
-    <Box
-      height="80vh"
-      width="80%"
-      margin="2rem"
-      alignItems="space-between"
-      justifyContent="center"
-      backgroundColor="#f2f5f2"
-      borderColor="green"
-    >
+class VideoBox extends React.Component {
+  state = {
+    openCamera: false,
+  }
+  cameraOn = () => {
+    this.setState({
+      openCamera: !this.state.openCamera
+    })
+  }
+  render() {
+    const {openCamera} = this.state
+    return (
       <Box
-        border="2px solid black"
-        height="90%"
-        width="100%"
-        marginBottom="0"
-        position="relative"
+        height="80vh"
+        width="80%"
+        margin="2rem"
+        alignItems="space-between"
+        justifyContent="center"
+        backgroundColor="#f2f5f2"
+        borderColor="green"
       >
-        User image goes here
         <Box
           border="2px solid black"
-          height="28%"
-          width="40%"
-          bottom="7px"
-          right="2"
-          position="absolute"
+          height="90%"
+          width="100%"
+          marginBottom="0"
+          position="relative"
         >
-          User2 image goes here
+          {openCamera ? <Webcam mirrored="true" imageSmoothing="true" /> : <div><p>Please Turn On your Video</p><br/></div>}
+          <button onClick={this.cameraOn}>{openCamera ? "Turn Camera Off" : 'Turn Camera On'}</button>
+          <Box
+            border="2px solid black"
+            height="28%"
+            width="40%"
+            bottom="7px"
+            right="2"
+            position="absolute"
+          >
+            User2 image goes here
         </Box>
+        </Box>
+        <Box
+          border="2px solid black"
+          height="10%"
+          width="100%"
+          position="relative"
+        >
+          Option buttons go here
       </Box>
-      <Box
-        border="2px solid black"
-        height="10%"
-        width="100%"
-        position="relative"
-      >
-        Option buttons go here
       </Box>
-    </Box>
-  );
+    );
+  }
 };
 
 export default VideoBox;
